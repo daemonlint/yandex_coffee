1888da40a47adad4837c64418a958ef70f5a80db
tse-egor <82537848+tse-egor@users.noreply.github.com>
2021-11-23 20:39:11 +0300

    some

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sqlite3
 connection = sqlite3.connect("coffee.sqlite")
 cursor = connection.cursor()
 
-result = cursor.execute("SELECT * FROM coffee").fetchall()
+response = cursor.execute("SELECT * FROM coffee").fetchall()
 
-print(result)
+print(response)
